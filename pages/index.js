2f92a8d24f0078d3c06a9f22c1de46b9b660a37d
PyPy <skfelix1@gmail.com>
2022-02-16 00:54:56 -0800

    adds copy to clipboard excel format

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import SearchBar from '../components/SearchBar'
 import Table from '../components/Table'
 import { useState, useEffect } from 'react'
 import { connectToDatabase } from '../utils/dbConnect'
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { ClipboardIcon, ClipboardCheckIcon } from '@heroicons/react/outline'
 
 
 
@@ -23,15 +25,7 @@ export default function Home({ count }) {
   const [loading, setLoading] = useState(false)
   const [result, setResult] = useState([])
   const [queue, setQueue] = useState([])
-
-function vibrate() {
-  if ("vibrate" in navigator) {
-    // vibration API supported
-    console.log("vibrated")
-    navigator.vibrate(1000);
-  }
-}
-
+  const [copy, setCopy] = useState(true)
 
   async function searchMongoDB(userQuery){
     if (userQuery?.length) {
@@ -50,8 +44,19 @@ function vibrate() {
 
 useEffect(() => {
   searchMongoDB(queue.slice(-1)[0] ) //search 1st upc in queue
-  vibrate()
 }, [queue])
+
+
+  function copyToClip() { //returns string that can be copied into excel
+    let returnSting = ''
+    result.forEach(item => {
+      const tab = "\t"
+      let entry = (item.upc) + tab + item.title + tab + item.size + tab + item.color2 + "\n"
+      returnSting += entry
+    })
+    console.log("🚀 ~ file: index.js ~ line 67 ~ copyToClip ~ returnSting", returnSting)
+    return returnSting
+  }
 
 
   return (
@@ -59,6 +64,27 @@ useEffect(() => {
       <div className=" max-w-7xl mx-auto px-7 sm:px-20 lg:px-8">
           <div className="max-w-3xl mx-auto ">
             <SearchBar count={count} query={query} setResult={setResult} setQuery={setQuery} queue={queue} setQueue={setQueue} />
+            
+            
+            <CopyToClipboard
+              options={{format: "text/plain"}}
+              text={copyToClip()}
+              >
+                {
+                  copy ? 
+                  <ClipboardIcon 
+                  onClick={() => setCopy(!copy)}
+                  className="h-6 w-6 rounded-full text-slate-500 hover:cursor-pointer m-3" />
+                  : 
+                  <ClipboardCheckIcon 
+                  onClick={() => setCopy(!copy)}
+                  className="h-6 w-6 rounded-full text-slate-500 hover:cursor-pointer m-3" />
+                  
+                }
+                 
+            </CopyToClipboard>
+
+
             <Table result={result} setResult={setResult} loading={loading}/>
           </div>
       </div>
