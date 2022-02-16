1f98e717217f4d08dd23009a5a98afa4a7bed01a
PyPy <skfelix1@gmail.com>
2022-02-16 00:00:44 -0800

    env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_DB: process.env.MONGODB_DB,
+  },
 }
