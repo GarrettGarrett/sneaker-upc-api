f7bfd0ae7ce3e909602efe576330b443c84550f7
PyPy <skfelix1@gmail.com>
2022-02-19 17:41:37 -0800

    handle api some upc codes start with ' some dont

--- pages/api/upc.js
+++ pages/api/upc.js
@@ -9,7 +9,16 @@ export default async (req, res) => {
         console.log("🚀 ~ file: upc.js ~ line 9 ~ upc", upc)
         
         const { db } = await connectToDatabase();
-        const mongo_response = await db.collection("sneakers").find({ 'upc' : "'" + upc }).toArray();
+        const mongo_response1 = await db.collection("sneakers").find({ 'upc' : "'" + upc }).toArray(); //some entries start with ' and some dont
+        const mongo_response2 = await db.collection("sneakers").find({ 'upc' : "'" + upc }).toArray(); //some entries start with ' and some dont
+
+        let mongo_response
+        if (mongo_response1.length > 0) {
+            mongo_response = mongo_response1
+        } else {
+            mongo_response = mongo_response2
+        }
+        
         const count = await db.collection("sneakers").count()
         console.log("🚀 ~ file: upc.js ~ line 14 ~ count", count)
         
