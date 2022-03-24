dbe4cc6fb8f63ccf81ff645c936a17a30d69b49b
PyPy <skfelix1@gmail.com>
2022-03-24 15:51:53 -0700

    adds dark mode to sitemap page

--- components/SitemapTimeline.js
+++ components/SitemapTimeline.js
@@ -42,14 +42,14 @@ export default function SitemapTimeline() {
             <li key={event.id} className='hover:cursor-pointer'>
                 <div className="relative pb-8">
                 {eventIdx !== timeline.length - 1 ? (
-                    <span className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
+                    <span className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-transparent" aria-hidden="true" />
                 ) : null}
                 <div className="relative flex space-x-3">
                     <div>
                     <span
                         className={classNames(
                         event.iconBackground,
-                        'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white'
+                        'h-8 w-8 rounded-full flex items-center justify-center ring-4 ring-white'
                         )}
                     >
                         <event.icon className="h-5 w-5 text-white" aria-hidden="true" />
@@ -57,7 +57,7 @@ export default function SitemapTimeline() {
                     </div>
                     <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
                     <div>
-                        <p className="text-sm text-gray-500">
+                        <p className="pl-2 text-sm text-white ">
                         {event.content}{' '}
                         
                         </p>
--- pages/sitemap.js
+++ pages/sitemap.js
@@ -11,10 +11,13 @@ function sitemap() {
           <meta name="description" content="Scan Sneaker Barcodes to Quickly Create Inventory Lists then Export to Excel, Notes, etc." />
           <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👟</text></svg>"/>
         </Head>
-    
+        <div className="bg-black mx-auto px-7 sm:px-20 lg:px-8">
+          <div className=" max-w-6xl mx-auto ">
         <div className='h-screen'>
-          <h1 className="pt-5 font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black">Site Map</h1>
+        <h1 className="relative pt-6 text-2xl font-extrabold leading-snug  uppercase lg:text-5xl tracking-widest font-display text-left text-white">Site Map</h1>
           <SitemapTimeline />          
+        </div>
+        </div>
         </div>
     
     </>
